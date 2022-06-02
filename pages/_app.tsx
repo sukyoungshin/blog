b8052b2c474ae209bd9fdbea0d4c1877c588e6e9
suKyoung <sukyoung_shin@kolon.com>
2022-06-02 23:15:04 +0900

    feat: theme 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '@styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+import { lightModeTheme } from '@styles/theme'
 import type { AppProps } from 'next/app'
+import React from 'react'
 
+// FIXME: ThemeProvider 제대로 된거 맞는지 체크
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightModeTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
