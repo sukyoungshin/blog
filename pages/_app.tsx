7d7cd345b13b944e479d8bcb752fb130a08f4367
suKyoung <sukyoung_shin@kolon.com>
2022-06-03 17:35:57 +0900

    feat: 프로젝트 FE 초기셋팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
 import '@styles/globals.css'
 import { ThemeProvider } from 'styled-components'
-import { lightModeTheme } from '@styles/theme'
+import { defaultTheme } from '@styles/theme'
 import type { AppProps } from 'next/app'
 import React from 'react'
 
-// FIXME: ThemeProvider 제대로 된거 맞는지 체크
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightModeTheme}>
+    <ThemeProvider theme={defaultTheme}>
       <Component {...pageProps} />
     </ThemeProvider>
   )
