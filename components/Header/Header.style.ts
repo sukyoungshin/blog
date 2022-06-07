a636b344a89029eb934ed4f1d8449d9e16b8d66b
suKyoung Shin <sukyoung.dev@gmail.com>
2022-06-07 19:28:41 +0900

    Squashed commit of the following:
    commit b9e7cf69d0d386bc112072e6850b810c92c99ed8
    Author: suKyoung Shin <sukyoung.dev@gmail.com>
    Date:   Tue Jun 7 16:20:49 2022 +0900
    
        feat: 공통 컴포넌트 Header 작업중

---
+++ components/Header/Header.style.ts
@@ -0,0 +1,14 @@
+import styled, { css } from "styled-components";
+
+export const StyledHeader = styled.header`
+  padding: 12px 24px;
+  width: 100%;
+  height: 56px;
+  min-height: 56px;
+  max-height: 56px;
+`;
+
+export const StyledNav = styled.nav`
+  width: 100%;
+  height: 100%;
+`;
