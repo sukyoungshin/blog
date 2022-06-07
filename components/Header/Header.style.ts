3db52059bffca06996b0d09b56b91bf914f6c1a6
suKyoung Shin <sukyoung.dev@gmail.com>
2022-06-07 19:59:01 +0900

    feat: 공통 컴포넌트 헤더작업중

--- components/Header/Header.style.ts
+++ components/Header/Header.style.ts
@@ -1,4 +1,5 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+import { COLOR } from "@styles/color";
 
 export const StyledHeader = styled.header`
   padding: 12px 24px;
@@ -6,9 +7,21 @@ export const StyledHeader = styled.header`
   height: 56px;
   min-height: 56px;
   max-height: 56px;
+  background-color: ${COLOR.WHITE_100};
 `;
 
 export const StyledNav = styled.nav`
   width: 100%;
   height: 100%;
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  // FIXME: 차후 로고 수정
+  span {
+    width: 80px;
+    height: 32px;
+    background-color: ${COLOR.WHITE_200};
+  }
 `;
