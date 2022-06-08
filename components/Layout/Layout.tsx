f25214a2b630452ace6a1ed4361eb36f5717ad0f
suKyoung Shin <sukyoung.dev@gmail.com>
2022-06-08 11:00:13 +0900

    feat: Layout 및 Header 로직수정

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,13 +3,27 @@ import Header from "@components/Header/Header";
 import Main from "@components/Main/Main";
 import Toolbar from "@components/Toolbar/Toolbar";
 
-export const Layout = ({ children }: any): ReactElement => {
+const Layout = ({ children, ...props }: any): ReactElement => {
 
-  return (
-    <>
-      <Header />
+  // 부모 컴포넌트에서 전달된 props값이 없으면, 기본헤더를 보여준다.
+  if (props.prev === '') {
+    return (
+      <>
+      <Header default />
       <Main>{children}</Main>
-      <Toolbar />
-    </>
-  );
+      <Toolbar default />
+    </>      
+    );
+  } else {
+    // 부모 컴포넌트에서 전달된 props값이 있으면, 뒤로가기 헤더를 보여주고 푸터는 생략한다.
+    return (
+      <>
+        <Header />
+        <Main>{children}</Main>
+      </>
+    );
+  }
+
 };
+
+export default Layout;
