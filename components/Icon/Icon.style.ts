634f3e8a3de1e10689dfde3e8eeb4d155cda41a8
suKyoung Shin <sukyoung.dev@gmail.com>
2022-06-10 18:59:22 +0900

    feat: Icon 컴포넌트 로직 및 css수정

--- components/Icon/Icon.style.ts
+++ components/Icon/Icon.style.ts
@@ -30,3 +30,32 @@ export const StyledIcon = styled.span<{
   `}
 `;
 
+export const StyledNormalIcon = styled.span<{
+  size?: number;
+  active?: boolean;
+}>`
+  // default
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transition: all 0.4s; // FIXME
+
+  // active & inactive
+  color: ${(props) => props.active? props.theme.colors.NAVY : props.theme.colors.WHITE_300};
+  background-color: none;
+
+  // size
+  ${(props) => props.size && css`  
+    width: ${props.size}px;
+    height: ${props.size}px;
+    border-radius: ${props.size / 2}px;
+
+    svg {
+      // svg size : 16x16 (minimum)
+      width: ${props.size-16 > 16 ? props.size-16 : 16}px;
+      height: ${props.size-16 > 16 ? props.size-16 : 16}px;
+    }
+  `}
+`;
--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -1,24 +1,39 @@
 import { useRouter } from "next/router";
 import { ReactElement } from "react";
-import { StyledIcon } from "./Icon.style";
+import { StyledIcon, StyledNormalIcon } from "./Icon.style";
 
 interface Props {
   children: ReactElement;
   size: number;
   route?: string;
+  active?: boolean;
 }
 
 const Icon = ({children, ...props}: Props) => {  
   const router = useRouter();
 
-  return (
-    <StyledIcon 
-      size={props.size}
-      active={props.route === router.route}
-    >
-      {children}
-    </StyledIcon>
-  )
+  // 부모요소에서 route요소를 내려준 경우 (Toolbar)
+  if (props.route) {
+    return (
+      <StyledIcon 
+        size={props.size}
+        active={props.route === router.route}
+      >
+        {children}
+      </StyledIcon>
+    )
+  } else {
+    // 부모요소에서 route요소를 내려주지 않은 경우
+    return (
+      <StyledNormalIcon 
+        size={props.size}
+        active={props.active}
+      >
+        {children}
+      </StyledNormalIcon>
+    )
+  };
+
 };
 
 export default Icon;
