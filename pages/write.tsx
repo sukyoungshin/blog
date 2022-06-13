f6f802d5a5324021eb5a9dc669ee1ca12a5acebb
suKyoung Shin <sukyoung.dev@gmail.com>
2022-06-13 17:32:08 +0900

    feat: 텍스트에디터 연동 및 글쓰기페이지 작업

--- pages/write.tsx
+++ pages/write.tsx
@@ -1,10 +1,55 @@
-import { ReactElement, ReactNode } from "react";
-import { Layout } from '@components/index'
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+import { ReactElement, ReactNode, useState } from "react";
+import { Button, Layout } from '@components/index'
+import { ButtonWrapper, EditorWrapper, Form } from '@styles/pages/write';
 
+// FIXME: 
+// - 로그인안했을 시, 해당 페이지 접속불가
+// - 상단에 로그아웃 헤더로 변경 (로그인정보에 따라)
+// - Button 선택 시, DB연동한 액션보여주도록 로직추가
 const Write = () => {
+  const [ initialVal, setInitialVal ] = useState('여기에 입력하세요');
+  const handleWritePosting = (prevVal: any) => {
+    return setInitialVal(prevVal);
+  }
+
   return (
     <>
-      Write
+    <EditorWrapper>
+      <Editor
+        initialValue={initialVal}
+        previewStyle="vertical"
+        height="628px"
+        initialEditType="markdown"
+        useCommandShortcut={true}
+        onChange={handleWritePosting}
+      />
+    </EditorWrapper>
+      <Form>
+        <ButtonWrapper>
+          <Button 
+            type="button"
+            active={false}
+            onClick={(e: any) => {
+              e.preventDefault();
+              console.log('임시작성')}
+            }
+          >
+            임시작성
+          </Button>
+          <Button 
+            type="button"
+            active={initialVal.length !== 0}
+            onClick={(e: any) => {
+              e.preventDefault();
+              console.log('글쓰기 버튼')}
+            }
+          >
+            글쓰기
+          </Button>
+        </ButtonWrapper>
+      </Form>
     </>
   );
 }
---
+++ styles/pages/write.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  margin: 0 auto;
+  width: 342px;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const EditorWrapper = styled.div`
+  margin: 16px 0;
+  width: 100%;
+  height: 628px;
+`;
