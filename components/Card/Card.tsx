74b357e3dba0f18de96f96e316e8877079565c4f
suKyoung Shin <sukyoung.dev@gmail.com>
2022-06-21 10:21:31 +0900

    feat: json-server 설치 및 API 연동

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -2,86 +2,95 @@ import { ReactElement } from 'react';
 import { CardWrapper, ContentWrapper, ImageWrapper, TabMenu, TabMenuList } from "./Card.style";
 import { Button, Icon } from '@components/index'
 import Image from 'next/image';
-import { BsFillHeartFill } from 'react-icons/bs';
 import Link from 'next/link';
+import { BsFillHeartFill } from 'react-icons/bs';
+import { useAPI } from '@utils/useAPI';
 
+const API_URL = 'posts';
 const Card = (): ReactElement => {
-
-  // FIXME: 실 데이터가 없어서 하드코딩한 내용. 추후 삭제 예정 (11라인 - 13라인, 이미지)
-  const tempString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-  const tempTitle = tempString.slice(0, 25);
-  const tempContent = tempString.slice(0, 115);
-  const tempDate = new Date().toLocaleDateString();
-  const tempNumberOfComments = '10개의 댓글';
-  const tempUserName = '신수경';
+  const {loading, error, data} = useAPI(API_URL);  
+  if (loading) return <h1>로딩중</h1>;
+  if (error) return <h1>error</h1>;
+  if (data === null) return <h1>data null</h1>;
 
   return (
-    // FIXME: link수정
-    <Link href="/write">
-    <a>
-      <CardWrapper>
-        <ImageWrapper>
-          <Image 
-            src={'/cat.webp'}
-            width={320}
-            height={167}
-            alt="profile" 
-          />
-        </ImageWrapper>
+    // FIXME: postUrl변경 (db.json)
+    <>
+    {
+      data.map((d) => (
+      <Link 
+        key={data ? d.id : 0}
+        href={data ? d.postUrl : '/'}
+      >
+      <a>
+        <CardWrapper>
+          <ImageWrapper>
+            <Image 
+              src={`${d.imgSrc}`}
+              width={320}
+              height={167}
+              alt={`${d.title} 이미지`} 
+            />
+          </ImageWrapper>
 
-        <ContentWrapper>
-          <h2>{tempTitle}...</h2>
-          <p>{tempContent}...</p>
-          <p>
-            <span>{tempDate}</span>
-            {' '}
-            <span>{tempNumberOfComments}</span>
-          </p>
-        </ContentWrapper>
-        
-        <TabMenuList>
-          <TabMenu>
-            <ul>
-              <li>
-                <Image 
-                  src={'/cat.webp'}
-                  width={24}
-                  height={24}
-                  style={{ borderRadius: '50%'}}
-                  alt="프로필사진" 
-                />
-              </li>
-              <li>
-                by{' '}
-                <strong>{tempUserName}</strong>
-              </li>
-            </ul>
-          </TabMenu>
+          <ContentWrapper>
+            <h2>{d.title.slice(0, 25)}...</h2>
+            <p>{d.content.slice(0, 115)}...</p>
+            <p>
+              <span>{d.createDate}</span>
+              {' '}
+              <span>{d.numberOfComments}개의 댓글</span>
+            </p>
+          </ContentWrapper>
+          
+          <TabMenuList>
+            <TabMenu>
+              <ul>
+                <li>
+                  <Image 
+                    src={`${d.imgSrc}`}
+                    width={24}
+                    height={24}
+                    style={{ borderRadius: '50%'}}
+                    alt={`${d.userName}님의 프로필사진`}
+                  />
+                </li>
+                <li>
+                  by{' '}
+                  <strong>{d.userName}</strong>
+                </li>
+              </ul>
+            </TabMenu>
 
-          <TabMenu>
-            <li>
-              <LikeButton />
-            </li>
-          </TabMenu>
-        </TabMenuList>
-      </CardWrapper>
-    </a>
-    </Link>
+            <TabMenu>
+              <li>
+                <LikeButton {...d} />
+              </li>
+            </TabMenu>
+          </TabMenuList>
+        </CardWrapper>
+      </a>
+      </Link>
+      ))
+    }
+    </>
   );
 }
 
-const LikeButton = (): ReactElement => {
+const LikeButton = ({...props}: any): ReactElement => {
 
   return (
     <Button 
       svgAndText={true} 
       small={true} 
-      onClick={() => console.log('좋아요 버튼')}
+      onClick={() => console.log('좋아요 버튼', props.id)}
     >
       <Icon size={24} active={true}>
         <BsFillHeartFill />
       </Icon>
-      <span>0</span>
+      <span>
+        {props.numberOfLikes}
+      </span>
     </Button>
   );
 }
